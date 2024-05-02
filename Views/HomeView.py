1a170f12d84512f66eda2d029c758a2fc604a9ff
ChristianAldana <caldanas2@miumg.edu.gt>
2024-05-02 10:15:57 -0600

    PRIMERA VERSION FRONT END

---
+++ Views/HomeView.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+class HomeView:
+
+    def home(self):
+        plantilla = get_template('index.html')
+        return HttpResponse(plantilla.render())
+
+    def formulario(self):
+        plantilla = get_template('formulario.html')
+        return HttpResponse(plantilla.render())
+
+    def agregar_paciente(self):
+        plantilla = get_template('agregarPaciente.html')
+        return HttpResponse(plantilla.render())
+
+    def editar_paciente(self):
+        plantilla = get_template('editarPaciente.html')
+        return HttpResponse(plantilla.render())
+
+    def eliminar_paciente(self):
+        plantilla = get_template('eliminarPaciente.html')
+        return HttpResponse(plantilla.render())
+
+    def datos_doctor(self):
+        plantilla = get_template('datosDoctor.html')
+        return HttpResponse(plantilla.render())
+
+    def editar_doctor(self):
+        plantilla = get_template('editarDoctor.html')
+        return HttpResponse(plantilla.render())
+
+
+    def agregar_doctor(self):
+        plantilla = get_template('agregarDoctor.html')
+        return HttpResponse(plantilla.render())
+
+
+    def eliminar_doctor(self):
+        plantilla = get_template('eliminarDoctor.html')
+        return HttpResponse(plantilla.render())
+
+
+
+
+
+
+
+
+
+
+
+
